d6905da3f9e04e1fd6d0834de13d2f83e9b890f3
sangita53 <glitchm0nst3r25@gmail.com>
2024-05-27 07:27:29 +0530

    Update CPU_FCFSalgo.c

--- CPU_FCFSalgo.c
+++ CPU_FCFSalgo.c
@@ -64,3 +64,55 @@ int main()
 
 
 //input for 3 process : 10 5 8   && for 5 process :  10 3 5 2 8 
+
+// #include <stdio.h>
+
+// typedef struct Process {
+//     int pid;
+//     int burst_time;
+//     int arrival_time;
+//     int turnaround_time;
+//     int waiting_time;
+// } Process;
+
+// void fcfs(Process processes[], int n, float *avg_turnaround_time, float *avg_waiting_time) {
+//     int time = 0;
+//     float total_turnaround_time = 0, total_waiting_time = 0;
+
+//     for (int i = 0; i < n; i++) {
+//         if (processes[i].arrival_time > time) {
+//             time = processes[i].arrival_time;
+//         }
+//         processes[i].waiting_time = time - processes[i].arrival_time;
+//         time += processes[i].burst_time;
+//         processes[i].turnaround_time = time - processes[i].arrival_time;
+//         total_turnaround_time += processes[i].turnaround_time;
+//         total_waiting_time += processes[i].waiting_time;
+//     }
+
+//     *avg_turnaround_time = total_turnaround_time / n;
+//     *avg_waiting_time = total_waiting_time / n;
+// }
+
+// int main() {
+//     // Example usage
+//     Process processes[] = {
+//         {1, 5, 0, 0, 0},
+//         {2, 3, 2, 0, 0},
+//         {3, 8, 4, 0, 0},
+//         {4, 6, 6, 0, 0}
+//     };
+//     int n = sizeof(processes) / sizeof(processes[0]);
+//     float avg_turnaround_time, avg_waiting_time;
+
+//     fcfs(processes, n, &avg_turnaround_time, &avg_waiting_time);
+
+//     printf("PID\tArrival Time\tBurst Time\tTurnaround Time\tWaiting Time\n");
+//     for (int i = 0; i < n; i++) {
+//         printf("%d\t%d\t\t%d\t\t%d\t\t%d\n", processes[i].pid, processes[i].arrival_time, processes[i].burst_time, processes[i].turnaround_time, processes[i].waiting_time);
+//     }
+
+//     printf("First Come First Serve (FCFS) Algorithm - Average Turnaround Time: %.2f, Average Waiting Time: %.2f\n", avg_turnaround_time, avg_waiting_time);
+
+//     return 0;
+// }
